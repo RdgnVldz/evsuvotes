0538a5527d322399f3b897871a92eb692056ae5e
Marco Pantonial <transpotac@gmail.com>
2023-05-28 05:39:39 +0800

    upload

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All Rights Reserved.</b>
+    </div>
+    <strong>&copy; 2023 <a style="color: white;" href="https://www.evsu.edu.ph/">Estern Visayas State University</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All Rights Reserved.</b>
+      </div>
+      <strong>&copy; 2023 <a style="color: white;" href="https://www.evsu.edu.ph/">Estern Visayas State University</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
